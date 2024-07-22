062329a6e4118964e369e958de24066db2459551
Tandat <buidat027@gamil.com>
2024-07-22 14:24:24 +0700

    config viewengine with template engine ejs, config folder route, render route main.ejs, config  css for view, create  folders use mvc

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// backend/index.js
+require('dotenv').config();
 const express = require('express');
+const configViewEngine = require('./src/config/viewEngine');
+const router = require('./src/routes/web');
+
 const app = express();
 const port = 3000;
+const hostname = process.env.HOST_NAME;
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'connected with Express sucessfuly!' });
-});
+//config template engine
+configViewEngine(app);
 
-app.listen(port, () => {
+//khai báo route
+app.use('/', router);
+
+// app.get('/api', (req, res) => {
+//   res.send({ message: 'connected with Express sucessfuly!' });
+// });
+
+app.listen(port,hostname, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
